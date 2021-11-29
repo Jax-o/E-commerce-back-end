0ea22abd9d953da816ebcf7fd7a58d17dee8d39b
Jacklyn Malone <jjmlmhc.09@gmail.com>
2021-11-29 15:47:48 -0500

    added models

--- models/Category.js
+++ models/Category.js
@@ -25,6 +25,16 @@ class Category extends Model { }
 Category.init(
 	{
 		// define columns
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		category_name: {
+			type: DataTypes.STRING,
+			allowNull: false
+		}
 	},
 	{
 		sequelize,
