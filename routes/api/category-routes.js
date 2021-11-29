72b24533d6c5b9f2c118cda34b65cc10b3973463
Jacklyn Malone <jjmlmhc.09@gmail.com>
2021-11-29 17:08:29 -0500

    working on routes to work

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,12 +7,11 @@ router.get('/', (req, res) => {
 	// find all categories
 	// be sure to include its associated Products
 	try {
-		const categoryData = await Category.findAll(), {
-			include: [{ model: Location, through: Trip, as: 'planned_trips' }]
-    });
-		}
-		res.status(200).json(travellerData);
-	} catch (err) {
+		const categoryData = await Category.findAll({
+			include: [{ model: Product }]
+		});
+	res.status(200).json(categoryData);
+	}	catch (err) {
 		res.status(500).json(err);
 	}
 });
@@ -20,18 +19,55 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
 	// find one category by its `id` value
 	// be sure to include its associated Products
+	try {
+		const categoryData = await Category.findByPk(req.params.id, {
+			include: [{ model: Product }]
+		});
+	res.status(200).json(categoryData);
+	}	catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.post('/', (req, res) => {
 	// create a new category
+	try {
+		const categoryData = await Category.create(req.body);
+		res.status(200).json(categoryData);
+	}	catch (err) {
+		res.status(400).json(err);
+	}
 });
 
 router.put('/:id', (req, res) => {
 	// update a category by its `id` value
+	Category.update(
+		{
+		  // Gets the category based on the id value in the request parameters
+		  where: {
+			 id: req.params.id,
+		  },
+		}
+	 )
+		.then((updatedCategory) => {
+		  // Sends the updated category as a json response
+		  res.json(updatedCategory);
+		})
+		.catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
 	// delete a category by its `id` value
+	try {
+		const categoryData = await Category.destroy({
+		  where: {
+			id: req.params.id
+		  }
+		});
+		res.status(200).json(categoryData);
+	 } catch (err) {
+		res.status(500).json(err);
+	 }
 });
 
 module.exports = router;
